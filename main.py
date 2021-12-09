d4a497f6abb15a23dfaf81f2fbf3828bd5df80f1
paramasivaanand <c.paramasivaanand@gmail.com>
2021-12-09 08:23:54 +0530

    class2

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-print("Anand again")
-a=10
-b=15
-c=a+b
-print("the value of c is",c)
-print(a>b)
-print(a//b)
-print(a%b)
-print(a>b and a<b)
+# print("Anand again")
+# a=10
+# b=15
+# c=a+b
+# print("the value of c is",c)
+# print(a>b)
+# print(a//b)
+# print(a%b)
+# print(a>b and a<b)
+# print (c+100)
+# print(c*100)
+# print(c*100)
+
+# a=int(input("enter the value a="))
+# b=int(input("enter the value b="))
+# c=a+b
+# print("the result is",c)
+# print("a" in "anand")
+# a=int(input("Enter the value of A"))
+# b=int(input("Enter the value of B"))
+# if(a>b):
+#     print("A is greater than B")
+# elif(a<b):
+#     print(" A is less than B")
+# else:
+#     print("A is not greater than B")
+a=int(input("enter tha value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter tha value of c"))
+if(a>=b) and (a>=c):
+    largest = a
+elif(b>=a) and (b>=c):
+    largest = b
+else:
+    largest = c
+print("c is greater",largest)
+
+
+
+
+
+
