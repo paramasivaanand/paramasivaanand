6876705af0f6110eb883ae295f53b1538dbacc6e
paramasivaanand <c.paramasivaanand@gmail.com>
2021-12-09 22:51:09 +0530

    new install

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("anand")
+# print("anand")
+# year=int(input("Enter the year"))
+# if(year%4)==0:
+#     if(year%100)==0:
+#         if(year%400)==0:
+#             print("the year is leap year")
+#         else:
+#             print("The year is not leap year")
+#     else:
+#         print("The year is leap year")
+# else:
+#  print("The year is not leap year")
+# a=int(input("enter the number"))
+# if a%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+# a=int(input("Enter the valu"))
+# if (a==1):
+#     print("The value is ",a)
+# elif(a==10):
+#     print("The value is ",a)
+# else:
+#     print("The value is not equal")
+# a=int(input("Enter the value of a"))
+# b=int(input("enter the value of b"))
+# c=int(input("Enter the value of c"))
+# if(a>=b)and(a>=c):
+#     print(" A is greatest")
+# elif(b>=a)and(b>=c):
+#     print("B is greatest")
+# else:
+#     print("c is greatest")
+# year=int(input("Enter the year"))
+# if year%4==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print("The year is leap year")
+#         else:
+#             print("The year is not leap year")
+#     else:
+#         print("The year is leap year")
+# else:
+#     print("The year is not leap year")
