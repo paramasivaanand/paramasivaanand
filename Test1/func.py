af417ee8a2274a04ed730de15d08880410af594c
paramasivaanand <c.paramasivaanand@gmail.com>
2022-02-05 09:22:27 +0530

    connect with DB

--- Test1/func.py
+++ Test1/func.py
@@ -1,8 +1,28 @@
-def add(a,b):
-    c=a+b
-    return c
-# ad=add(10,20)
-# print(ad)
-def sub(a,b):
-    c=a-b
-    return c
+# def add(a,b):
+#     c=a+b
+#     return c
+# # ad=add(10,20)
+# # print(ad)
+# def sub(a,b):
+#     c=a-b
+#     return c
+
+
+import pymysql
+
+conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password ="",
+        db='testdb',
+        )
+db=conn.cursor()
+sql = "INSERT INTO persons (FirstName, Address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+db.execute(sql, val)
+conn.commit()
+db.execute("SELECT * FROM `persons` WHERE 1;")
+t=db.fetchall()
+
+print(t)
+db.close()
